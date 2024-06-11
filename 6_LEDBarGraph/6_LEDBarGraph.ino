ae8e99825222bfd300c0e83a8ba5bb5b2b055dbf
Aryan-Prakhar <97614212+Aryan-Prakhar@users.noreply.github.com>
2024-06-11 11:46:52 +0530

    Update 6_LEDBarGraph.ino

--- 6_LEDBarGraph/6_LEDBarGraph.ino
+++ 6_LEDBarGraph/6_LEDBarGraph.ino
@@ -5,7 +5,7 @@
 // please support Upside Down Labs and open-source hardware by purchasing
 // products from Upside Down Labs!
 
-// Copyright (c) 2021 - 2023 Upside Down Labs - contact@upsidedownlabs.tech
+// Copyright (c) 2021 - 2024 Upside Down Labs - contact@upsidedownlabs.tech
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
